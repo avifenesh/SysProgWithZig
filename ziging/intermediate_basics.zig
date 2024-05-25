e6d660b753769ac8cc0870e2087a423117b542a0
Avi Fenesh <aviarchi1994@gmail.com>
2024-05-25 16:13:56 +0000

    integer rules

--- ziging/intermediate_basics.zig
+++ ziging/intermediate_basics.zig
@@ -1,8 +1,10 @@
 const expect = @import("std").testing.expect;
 const expectEqual = @import("std").testing.expectEqual;
+const expectError = @import("std").testing.expectError;
 const std = @import("std");
+const math = std.math;
 
-// Slices
+// # Slices #
 
 fn total(values: []const u8) usize {
     var sum: usize = 0;
@@ -29,7 +31,7 @@ test "slicing till end or till length" {
     try expect(slice1.len == slice2.len);
 }
 
-// Enums
+// # Enums #
 
 const Direction = enum { north, south, west, east };
 
@@ -64,7 +66,7 @@ test "globally count" {
     try expectEqual(Mode.increase_count_and_return(), 2);
 }
 
-// Structs
+// # Structs #
 
 const FamilyAges = struct {
     me: u16 = 29,
@@ -82,7 +84,7 @@ test "some games with structs" {
     try expect(myAges.me == myAges.she);
 }
 
-// Unions
+// # Unions #
 
 const Result = union { int: i64, float: f64, bool: bool };
 
@@ -112,3 +114,66 @@ test "switch on tagged union" {
 }
 
 const AnotherDirect = union(enum) { north: u8, south: i64, west: f32, east: bool, none };
+
+// # Integer Rules #
+
+const decimal_integer: i32 = 12;
+const octal_integer: i32 = 0o14;
+const hex_integer: i32 = 0xC;
+const binary_integer: i32 = 0b1100;
+
+test "integer rules" {
+    try expectEqual(decimal_integer, octal_integer);
+    try expectEqual(hex_integer, binary_integer);
+    try expect(decimal_integer == hex_integer and hex_integer == octal_integer and octal_integer == binary_integer);
+}
+
+// Under score in integer literals
+
+const underscored_integer: i32 = 1_000_000;
+const underscored_float: f32 = 1_000_000.0;
+const binary_mask: i32 = 0b1111_0100_0010_0100_0000;
+
+test "underscore in integer literals" {
+    try expectEqual(underscored_integer, 1000000);
+    try expectEqual(underscored_float, 1000000.0);
+    try expectEqual(binary_mask, 0b1111_0100_0010_0100_0000);
+    try expectEqual(underscored_integer, binary_mask);
+    try expectEqual(underscored_float, binary_mask);
+    try expectEqual(underscored_integer, underscored_float);
+}
+
+// Integer widening
+
+const IntsError = error{OverFlow};
+
+fn addOne(variable: i8) IntsError!void {
+    if (variable == math.maxInt(i8)) return IntsError.OverFlow;
+    var new_var: i8 = variable;
+    new_var += 1;
+}
+
+test "integer widening" {
+    const i8_val: i8 = 127;
+    const i16_val: i16 = i8_val;
+    const i32_val: i32 = i16_val;
+    const i64_val: i64 = i32_val;
+    try expectEqual(i8_val, i16_val);
+    try expectEqual(i16_val, i32_val);
+    try expectEqual(i32_val, i64_val);
+    try expectEqual(addOne(i8_val), IntsError.OverFlow);
+}
+
+test "@intCast" {
+    const x: u64 = 200;
+    const y = @as(u8, @intCast(x));
+    try expectEqual(@TypeOf(y), u8);
+}
+
+// Overflow operators
+
+test "well defined overflow" {
+    var a: u8 = 255;
+    a +%= 1;
+    try expectEqual(a, 0);
+}
