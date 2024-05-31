b0f0dcdc99fee231f4c93a263a6ca70407985bbe
Avi Fenesh <aviarchi1994@gmail.com>
2024-05-31 07:33:21 +0000

    Payloads capture

--- ziging/advanced_basics.zig
+++ ziging/advanced_basics.zig
@@ -130,3 +130,95 @@ test "**" {
     const memory = pattern ** 3;
     try expect(eql(u8, &memory, &[_]u8{ 0xCC, 0xAA, 0xCC, 0xAA, 0xCC, 0xAA }));
 }
+
+// # Payloads Capture #
+
+test "optional if" {
+    const maybe_num: ?usize = 10;
+    if (maybe_num) |num| {
+        try expect(@TypeOf(num) == usize);
+        try expect(num == 10);
+    } else {
+        unreachable;
+    }
+}
+
+test "error union if" {
+    const ent_num: error{UnknownEntity}!u32 = 5;
+    if (ent_num) |num| {
+        try expect(@TypeOf(num) == u32);
+        try expect(num == 5);
+    } else |err| {
+        _ = err catch {};
+        unreachable;
+    }
+}
+
+test "while optional" {
+    var i: ?u32 = 10;
+    while (i) |num| : (i.? -= 1) {
+        try expect(@TypeOf(num) == u32);
+        if (num == 1) {
+            i = null;
+            break;
+        }
+    }
+    try expect(i == null);
+}
+
+var number_left2: u32 = undefined;
+
+fn eventuallyErrorSequence() !u32 {
+    return if (number_left2 == 0) error.ReachedZero else blk: {
+        number_left2 -= 1;
+        break :blk number_left2;
+    };
+}
+
+test "while error union capture" {
+    var sum: u32 = 0;
+    number_left2 = 3;
+    while (eventuallyErrorSequence()) |num| {
+        sum += num;
+    } else |err| {
+        try expect(err == error.ReachedZero);
+    }
+}
+
+test "for capture" {
+    const x = [_]i8{ 1, 5, 120, -5 };
+    for (x) |v| try expectEqual(@TypeOf(v), i8);
+}
+
+const Info = union(enum) {
+    a: u32,
+    b: []const u8,
+    c,
+    d: u32,
+};
+
+test "switch capture" {
+    const b = Info{ .a = 10 };
+    const x = switch (b) {
+        .b => |str| blk: {
+            try expectEqual(@TypeOf(str), []const u8);
+            break :blk 1;
+        },
+        .c => 2,
+        .a, .d => |num| blk: {
+            try expectEqual(@TypeOf(num), u32);
+            break :blk num * 2;
+        },
+    };
+    try expect(x == 20);
+}
+
+test "for with pointer capture" {
+    var data = [_]u8{
+        1,
+        2,
+        3,
+    };
+    for (&data) |*byte| byte.* += 1;
+    try expect(eql(u8, &data, &[_]u8{ 2, 3, 4 }));
+}
