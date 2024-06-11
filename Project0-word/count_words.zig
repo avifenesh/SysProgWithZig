533427c10ddc603439029f58ed0120ed6b8d815b
Avi Fenesh <aviarchi1994@gmail.com>
2024-06-11 15:19:35 +0000

    Finished task 0 including tests etc.

--- Project0-word/count_words.zig
+++ Project0-word/count_words.zig
@@ -1,14 +1,12 @@
 const std = @import("std");
-const heap = std.heap;
-const os_tag = @import("builtin").os.tag;
-const Reader = std.fs.File.Reader;
 const print = std.debug.print;
 const split = std.mem.splitScalar;
+const utils = @import("utils.zig");
 
-pub fn main(file_path: []const u8) void {
+pub fn countWords(file_path: []const u8) u64 {
     const file = std.fs.cwd().openFile(file_path, .{ .mode = .read_only }) catch |err| {
         print("Error opening file: {}\n", .{err});
-        return;
+        return 0;
     };
     defer file.close();
     var buffer: [1024]u8 = undefined;
@@ -16,7 +14,7 @@ pub fn main(file_path: []const u8) void {
     const reader = file.reader();
     while (true) {
         var line: []const u8 = undefined;
-        const line_options = nextLine(reader, &buffer) catch |err| {
+        const line_options = utils.nextLine(reader, &buffer) catch |err| {
             print("Error reading line: {}\n", .{err});
             break;
         };
@@ -29,16 +27,5 @@ pub fn main(file_path: []const u8) void {
         while (words.next() != null) words_count += 1;
     }
     print("Words count: {}\n", .{words_count});
-}
-
-fn nextLine(reader: Reader, buffer: []u8) !?[]const u8 {
-    const line = reader.readUntilDelimiterOrEof(buffer, '\n') catch |err| {
-        print("Err: {}\n", .{err});
-        return err;
-    };
-    if (os_tag == .windows) {
-        return std.mem.trimRight(u8, line, '\r');
-    } else {
-        return line;
-    }
+    return words_count;
 }
